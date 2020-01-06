3fb3e04d2692e72e95e3ad534ed3fc5951f6693b
TanakaNozomi07 <non@DESKTOP-8PAEJO7.localdomain>
2020-01-06 15:35:58 +0900

    add file

--- assignments/MyRegression.py
+++ assignments/MyRegression.py
@@ -1,9 +1,33 @@
-
+import numpy as np
+import pandas as pd
+from sklearn.base import BaseEstimator, RegressorMixin
+from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
+a = pd.read_csv("../text/data/X_train.csv", sep=";")
+b = pd.read_csv("../text/data/y_train.csv", sep=";")
 
 class MyRegression():
-	def __init__(self):
-		pass
-
-
+    def __init__(self, lam = 0):
+        
+        self.a = a
+        self.b = b
+        self.lam = lam
+        
+    def fit(self, X, y):
+        X, y = check_X_y(X, y, y_numeric=True)
+        if self.lam != 0:
+            pass
+        else:
+            pass
+        
+        self.a_ =  np.linalg.solve(X, y)
+        self.b_ =  np.linalg.solve(X,y)
+        return self
+        
+    def predict(self, X):
+        check_is_fitted(self, "a_", "b_") # 学習済みかチェックする(推奨)
+        X = check_array(X)
+        return y
+        
 if __name__=="__main__":
-	clf = MyRegression()
+    clf = MyRegression()
+
