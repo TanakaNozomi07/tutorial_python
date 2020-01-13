298d45e2035cd1416e167ff630b993f1629d1389
TanakaNozomi07 <57595856+TanakaNozomi07@users.noreply.github.com>
2020-01-13 18:22:51 +0900

    Update MyRegression.py

--- assignments/MyRegression.py
+++ assignments/MyRegression.py
@@ -5,8 +5,8 @@ import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, RegressorMixin
 from sklearn.utils.validation import check_X_y, check_is_fitted, check_array
-a = pd.read_csv("tutorial_python/text/data/X_train.csv", sep=",")
-b = pd.read_csv("tutorial_python/text/data/y_train.csv", sep=",")
+a = pd.read_csv("../text/data/X_train.csv", sep=",")
+b = pd.read_csv("../text/data/y_train.csv", sep=",")
 
 class MyRegression():
     def __init__(self,lam = 0,a=None,b=None):
